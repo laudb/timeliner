4c56fb8d9860a1b0155880fc1fbdc22b6a27a8fb
Laud Bentil <laud.bentil@meltwater.org>
2014-11-27 17:08:12 +0000

    changes in Feed model, added Article scaffold

--- timeliner2/timeline/app/models/feed.rb
+++ timeliner2/timeline/app/models/feed.rb
@@ -4,32 +4,36 @@ class Feed < ActiveRecord::Base
 
     # to automate this process in future 
 
-    #f  - id of a given news url goes here. "1", "2", ...
-    #n  - name of a given news url. "the guardian", etc.
-    #u  - url of the given news url. "http://www.***.rss"
-    #c  - category of given news url. "sports", "tech", etc.
+    # f  - id of a given news url goes here. "1", "2", ...
+    # n  - name of a given news url. "the guardian", etc.
+    # u  - url of the given news url. "http://www.***.rss"
+    # c  - category of given news url. "sports", "tech", etc.
 
-    # create a feed, passing in values
-    # @feed = Feed.create(fid: f, name: n, url: u, category: c)
+    # create a database, passing in values
+    # Feed.create(fid: f, name: n, url: u, category: c)
   end
+
 
   def fetch_articles
     require 'open-uri'
     require 'simple-rss'
 
+    # takes url from Feed object and assigns it to url
     url = Feed.url
 
+    # opens url and assign to rss object
     rss = SimpleRSS.parse open(url)
 
-    # iterate through feed. Save title, link, description
+    # iterate through rss items, gathering title, link, description
     rss.items.each do |r|
 
       t = r.title.force_encoding("UTF-8")
       l = r.link.force_encoding("UTF-8")
       d = r.description.force_encoding("UTF-8")
 
-      #@article = Article.create(title: t, url: l, description: d)
-    end
-  end
+     # Create database entry parsing in the following params
+     # Article.create(title: t, url: l, description: d)
+   end
+ end
 end
 
---
+++ timeliner2/timeline/app/views/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :title, :url, :description, :created_at, :updated_at
